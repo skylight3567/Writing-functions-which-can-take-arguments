dac4618810dec768a664164851162c753dc2d1d5
skylight3567 <88144648+skylight3567@users.noreply.github.com>
2021-09-30 18:15:28 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var fixedRect, movingRect;
+
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "yellow";
+  //fixedRect.debug = true;
+
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "yellow";
+  //movingRect.debug = true;
+
+gameObject1 = createSprite(100,100,50,50);
+gameObject1.shapeColor="yellow";
+gameObject2 = createSprite(200,100,50,50);
+gameObject2.shapeColor="yellow";
+gameObject3 = createSprite(300,100,50,50);
+gameObject3.shapeColor="yellow";
+gameObject4 = createSprite(400,100,50,50);
+gameObject4.shapeColor="yellow";
+}
+
+function draw() {
+  background("magenta");  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+if(isTouching(movingRect, gameObject2)){
+  movingRect.shapeColor = "red";
+  gameObject2.shapeColor = "red";
+}
+else{
+  movingRect.shapeColor = "yellow";
+  gameObject2.shapeColor = "yellow";
+
+}
+  drawSprites();
+}
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+      return true;
+}
+else {
+      return false;
+}
+}
